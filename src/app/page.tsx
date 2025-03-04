fd24f4200ed7876ced8b27e0c3616eab0b52ee26
hederalves <alvesheder92@gmail>
2025-03-03 22:40:28 -0300

    feat: create modal to add new vital data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchHealthFlows } from '@/reducer/healthflowReducer';
+import { fetchHealthFlows, HealthFlow, updateHealthFlowFirebase } from '@/reducer/healthflowReducer';
 import { AppDispatch, RootState } from '@/store/store';
 import { GiHealthPotion } from 'react-icons/gi';
 import { FaThermometerHalf, FaHeartbeat, FaLungs, FaUserNurse } from 'react-icons/fa';
@@ -12,31 +12,84 @@ const HealthFlowList = () => {
     const dispatch = useDispatch<AppDispatch>();
     const { healthFlows, loading, error } = useSelector((state: RootState) => state.healthflow);
 
+    // Estado do modal
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedFlow, setSelectedFlow] = useState<HealthFlow | null>(null);
+    const [newVitalData, setNewVitalData] = useState({
+        temperature: '',
+        heartRate: '',
+        bloodPressure: '',
+        respiratoryRate: ''
+    });
+
     useEffect(() => {
         dispatch(fetchHealthFlows());
     }, [dispatch]);
+
+    const handleModalOpen = (flow: HealthFlow) => {
+        setSelectedFlow(flow);
+        setIsModalOpen(true);
+    };
+
+    const handleModalClose = () => {
+        setIsModalOpen(false);
+        setSelectedFlow(null);
+        setNewVitalData({
+            temperature: '',
+            heartRate: '',
+            bloodPressure: '',
+            respiratoryRate: ''
+        });
+    };
+
+    const handleSubmit = () => {
+        if (selectedFlow) {
+            const updatedFlow = {
+                ...selectedFlow,
+                vitalData: [
+                    ...(selectedFlow.vitalData || []), // Adiciona os dados vitais existentes
+                    {
+                        temperature: parseFloat(newVitalData.temperature),
+                        heartRate: parseInt(newVitalData.heartRate),
+                        bloodPressure: newVitalData.bloodPressure,
+                        respiratoryRate: parseInt(newVitalData.respiratoryRate),
+                        timestamp: new Date().toISOString(),
+                    }
+                ]
+            };
+
+            // Enviar os dados para o Firebase (atualizando os dados vitais)
+            dispatch(updateHealthFlowFirebase(updatedFlow));
+            handleModalClose();
+        }
+    };
 
     if (loading) return <p>Carregando...</p>;
     if (error) return <p>Erro ao carregar os dados: {error}</p>;
 
     return (
         <div className="p-4 flex">
-            {healthFlows.map((flow) => (
-                <div key={`${flow.id}-${flow.doctorId}`} className="bg-white rounded-2xl shadow-md m-2 w-[250px]">
+            {healthFlows.map((flow: HealthFlow) => (
+                <div
+                    key={`${flow.id}-${flow.doctorId}`}
+                    className="bg-white rounded-2xl shadow-md m-2 w-[250px]"
+                    onClick={() => handleModalOpen(flow)} // Abrir o modal ao clicar no card
+                >
                     <div className="p-2 rounded-t-2xl w-min-[250px]">
                         <div className="flex justify-between px-1 pt-1 border-b-2 border-black cursor-pointer">
                             <h3 className="text-xs font-semibold text-gray-500">Sinais Vitais</h3>
                             <h3 className="text-base font-semibold">{flow.patientName}</h3>
                         </div>
-                        {flow.vitalData && flow.vitalData.length > 0 && (
+                        {flow.vitalData && (
+                            // Exibir os dados vitais mais recentes
                             <div className="flex justify-between text-sm px-1 py-2">
                                 <div className="flex flex-col">
-                                    <span><GiHealthPotion className="text-purple-500 inline mr-2" /><strong>{flow.vitalData[0].bloodPressure} mmHg</strong></span>
-                                    <span><FaThermometerHalf className="text-yellow-500 inline mr-2" /><strong>{flow.vitalData[0].temperature}°C</strong></span>
+                                    <span><GiHealthPotion className="text-purple-500 inline mr-2" /><strong>{flow.vitalData[flow.vitalData.length - 1].bloodPressure} mmHg</strong></span>
+                                    <span><FaThermometerHalf className="text-yellow-500 inline mr-2" /><strong>{flow.vitalData[flow.vitalData.length - 1].temperature}°C</strong></span>
                                 </div>
                                 <div className="flex flex-col">
-                                    <span><FaHeartbeat className="text-red-500 inline mr-2" /><strong>{flow.vitalData[0].heartRate} bpm</strong></span>
-                                    <span><FaLungs className="text-blue-500 inline mr-2" /><strong>{flow.vitalData[0].respiratoryRate} mm</strong></span>
+                                    <span><FaHeartbeat className="text-red-500 inline mr-2" /><strong>{flow.vitalData[flow.vitalData.length - 1].heartRate} bpm</strong></span>
+                                    <span><FaLungs className="text-blue-500 inline mr-2" /><strong>{flow.vitalData[flow.vitalData.length - 1].respiratoryRate} mm</strong></span>
                                 </div>
                             </div>
                         )}
@@ -73,6 +126,73 @@ const HealthFlowList = () => {
                     </div>
                 </div>
             ))}
+
+            {/* Modal de Adição de Dados Vitais */}
+            {isModalOpen && selectedFlow && (
+                <div className="fixed inset-0 flex justify-center items-center bg-gray-500 bg-opacity-50 z-50">
+                    <div className="bg-white p-6 rounded-lg w-96">
+                        <h3 className="text-xl font-semibold mb-4">Adicionar Dados Vitais</h3>
+                        <form>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Temperatura (°C)</label>
+                                <input
+                                    type="number"
+                                    value={newVitalData.temperature}
+                                    onChange={(e) => setNewVitalData({ ...newVitalData, temperature: e.target.value })}
+                                    className="mt-1 p-2 w-full border border-gray-300 rounded"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Batimento Cardíaco (bpm)</label>
+                                <input
+                                    type="number"
+                                    value={newVitalData.heartRate}
+                                    onChange={(e) => setNewVitalData({ ...newVitalData, heartRate: e.target.value })}
+                                    className="mt-1 p-2 w-full border border-gray-300 rounded"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Pressão Arterial</label>
+                                <input
+                                    type="text"
+                                    value={newVitalData.bloodPressure}
+                                    onChange={(e) => setNewVitalData({ ...newVitalData, bloodPressure: e.target.value })}
+                                    className="mt-1 p-2 w-full border border-gray-300 rounded"
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Frequência Respiratória (mm)</label>
+                                <input
+                                    type="number"
+                                    value={newVitalData.respiratoryRate}
+                                    onChange={(e) => setNewVitalData({ ...newVitalData, respiratoryRate: e.target.value })}
+                                    className="mt-1 p-2 w-full border border-gray-300 rounded"
+                                    required
+                                />
+                            </div>
+                            <div className="flex justify-end gap-4">
+                                <button
+                                    type="button"
+                                    onClick={handleModalClose}
+                                    className="bg-gray-400 text-white px-4 py-2 rounded"
+                                >
+                                    Cancelar
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={handleSubmit}
+                                    className="bg-blue-500 text-white px-4 py-2 rounded"
+                                >
+                                    Salvar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
