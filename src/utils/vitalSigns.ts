9360add6a12022b6132c1a887834cd7dcf0e955b
hederalves <alvesheder92@gmail>
2025-03-21 02:39:44 -0300

    feat: modify references to vitalSigns

--- src/utils/vitalSigns.ts
+++ src/utils/vitalSigns.ts
@@ -6,14 +6,15 @@ export const vitalSignsReference: {
     [key in AgeGroup]: {
         heartRate: number[];
         respiratoryRate: number[];
-        bloodPressure: number[] | null;
+        bloodPressure: { minSystolic: number, maxSystolic: number, minDiastolic: number, maxDiastolic: number } | null;
         temperature: number[];
+        saturation: number[];
     };
 } = {
-    bebê: { heartRate: [100, 160], respiratoryRate: [30, 60], bloodPressure: [110, 75], temperature: [36.1, 37.2] },
-    criança: { heartRate: [80, 120], respiratoryRate: [20, 30], bloodPressure: [120, 80], temperature: [36.1, 37.2] },
-    adulto: { heartRate: [60, 100], respiratoryRate: [12, 20], bloodPressure: [139, 89], temperature: [36.1, 37.2] },
-    idoso: { heartRate: [45, 90], respiratoryRate: [16, 25], bloodPressure: null, temperature: [36.1, 37.2] },
+    bebê: { heartRate: [100, 160], respiratoryRate: [30, 60], bloodPressure: { minSystolic: 90, maxSystolic: 140, minDiastolic: 60, maxDiastolic: 99 }, temperature: [36.1, 37.2], saturation: [90, 100] },
+    criança: { heartRate: [80, 120], respiratoryRate: [20, 30], bloodPressure: { minSystolic: 90, maxSystolic: 140, minDiastolic: 60, maxDiastolic: 99 }, temperature: [36.1, 37.2], saturation: [90, 100] },
+    adulto: { heartRate: [60, 100], respiratoryRate: [14, 20], bloodPressure: { minSystolic: 90, maxSystolic: 140, minDiastolic: 60, maxDiastolic: 99 }, temperature: [35.6, 37.7], saturation: [90, 100] },
+    idoso: { heartRate: [45, 90], respiratoryRate: [16, 25], bloodPressure: null, temperature: [36.1, 37.2], saturation: [90, 100] },
 };
 
 export function countAbnormalVitalSigns(flow: HealthFlow) {
@@ -21,74 +22,62 @@ export function countAbnormalVitalSigns(flow: HealthFlow) {
     const patientGender = flow.patientGender;
 
     if (!patientAgeGroup || !vitalSignsReference.hasOwnProperty(patientAgeGroup)) {
-        console.error(`Idade do paciente '${patientAgeGroup}' não é válida ou não encontrada.`);
         return 0;
     }
 
     const vitalSigns = vitalSignsReference[patientAgeGroup];
-    console.log("------------------------------------------------------------");
-    console.log(`Sinais vitais de referência para ${patientAgeGroup}:`, vitalSigns);
 
     let bloodPressureReference = vitalSigns.bloodPressure;
     if (patientAgeGroup === "idoso") {
-        const originalBloodPressure = bloodPressureReference;
-        bloodPressureReference = patientGender === "mulher" ? [134, 84] : [135, 88];
-        console.log(`Pressão arterial ajustada para idoso (${patientGender}): de ${originalBloodPressure} para ${bloodPressureReference}`);
+        bloodPressureReference = patientGender === "mulher" ? { minSystolic: 90, maxSystolic: 140, minDiastolic: 60, maxDiastolic: 99 } : { minSystolic: 90, maxSystolic: 140, minDiastolic: 60, maxDiastolic: 99 };
     }
 
     if (!flow.vitalData || flow.vitalData.length === 0) {
-        console.log("Nenhum dado vital encontrado para este paciente.");
         return 0;
     }
 
     const lastVitalData = flow.vitalData[flow.vitalData.length - 1];
-    console.log(`Últimos dados vitais para ${patientAgeGroup}:`, lastVitalData);
-
     let bloodPressureArray: number[] = [];
     if (typeof lastVitalData.bloodPressure === "string") {
         const splitBP = lastVitalData.bloodPressure.split("/").map(bp => parseInt(bp.trim(), 10));
         if (splitBP.length === 2 && !isNaN(splitBP[0]) && !isNaN(splitBP[1])) {
             bloodPressureArray = splitBP;
-            console.log(`Pressão arterial convertida: ${bloodPressureArray}`);
         }
     }
 
     let abnormalCount = 0;
 
     if (lastVitalData.heartRate < vitalSigns.heartRate[0] || lastVitalData.heartRate > vitalSigns.heartRate[1]) {
-        console.log(`Frequência cardíaca alterada: ${lastVitalData.heartRate} (esperado: ${vitalSigns.heartRate[0]}-${vitalSigns.heartRate[1]})`);
         abnormalCount++;
     }
 
     if (lastVitalData.respiratoryRate < vitalSigns.respiratoryRate[0] || lastVitalData.respiratoryRate > vitalSigns.respiratoryRate[1]) {
-        console.log(`Frequência respiratória alterada: ${lastVitalData.respiratoryRate} (esperado: ${vitalSigns.respiratoryRate[0]}-${vitalSigns.respiratoryRate[1]})`);
         abnormalCount++;
     }
 
     if (lastVitalData.temperature < vitalSigns.temperature[0] || lastVitalData.temperature > vitalSigns.temperature[1]) {
-        console.log(`Temperatura alterada: ${lastVitalData.temperature} (esperado: ${vitalSigns.temperature[0]}-${vitalSigns.temperature[1]})`);
+        abnormalCount++;
+    }
+
+    if (lastVitalData.saturation < vitalSigns.saturation[0] || lastVitalData.saturation > vitalSigns.saturation[1]) {
         abnormalCount++;
     }
 
     if (bloodPressureArray.length === 2 && bloodPressureReference) {
         const [systolic, diastolic] = bloodPressureArray;
-        const [systolicRef, diastolicRef] = bloodPressureReference;
+        const { minSystolic, maxSystolic, minDiastolic, maxDiastolic } = bloodPressureReference;
 
-        let message = "Pressão alterada:";
         let isAbnormal = false;
 
-        if (systolic > systolicRef) {
-            message += ` Sistólica ${systolic} (esperado: ${systolicRef})`;
+        if (systolic < minSystolic || systolic > maxSystolic) {
             isAbnormal = true;
         }
 
-        if (diastolic > diastolicRef) {
-            message += ` Diastólica ${diastolic} (esperado: ${diastolicRef})`;
+        if (diastolic < minDiastolic || diastolic > maxDiastolic) {
             isAbnormal = true;
         }
 
         if (isAbnormal) {
-            console.log(message);
             abnormalCount++;
         }
     }
